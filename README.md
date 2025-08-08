75029e20d898992c7b45fd30ba05da8206cdd1ac
Ruchita06-sah <sahaneruchita06@gmail.com>
2025-08-08 11:57:58 +0000

    Initial commit with app.py, README, and .gitignore

---
+++ README.md
@@ -0,0 +1,4 @@
+# DevOps Task – Version Controlled Project with Git
+
+This project demonstrates branching, pull requests, tags, and documentation in Git.
+
---
+++ app.py
@@ -0,0 +1,7 @@
+def greet(name):
+    return f"Hello, {name}! Welcome to DevOps Task."
+
+if __name__ == "__main__":
+    user = input("Enter your name: ")
+    print(greet(user))
+
